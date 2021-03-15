0249e2f1057e016286811aea9ec53b3cb8942bbf
Azema4ka <co4nev.1081@mail.ru>
2021-03-15 17:47:15 +0300

    upgrade jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  setupFilesAfterEnv: ['<rootDir>/tests/unit/setup.js'],
+  verbose: true,
+  errorOnDeprecated: true,
+  clearMocks: true
 }
