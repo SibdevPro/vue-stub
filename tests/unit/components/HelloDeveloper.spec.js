56a13a3c80324e75e2cb4376965517f127da3b83
Azema4ka <co4nev.1081@mail.ru>
2021-03-16 11:33:48 +0300

    upgrade tests for component HelloDeveloper

--- tests/unit/components/HelloDeveloper.spec.js
+++ tests/unit/components/HelloDeveloper.spec.js
@@ -1,31 +1,42 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+import { shallowMount } from '@vue/test-utils'
 import Vuex from 'vuex'
 // Components
 import HelloDeveloper from '@/components/HelloDeveloper.vue'
 
+Vue.use(Vuex)
+
 describe('HelloDeveloper component', () => {
   let wrapper
+  let store
+
+  const DEFAULT_PROPS = {
+    developer: 'default value'
+  }
+
+  const DEFAULT_STORE_CONFIG = {
+    state: {
+      someState: {
+        loading: false
+      }
+    },
+    getters: {},
+    actions: {},
+    mutations: {}
+  }
 
   function initWrapper({ props } = {}) {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
+    const storeConfig = {
+      ...DEFAULT_STORE_CONFIG
+    }
 
-    const store = new Vuex.Store({
-      state: {
-        someState: {
-          loading: false
-        }
-      },
-      getters: {},
-      actions: {},
-      mutations: {}
-    })
+    store = new Vuex.Store(storeConfig)
 
     wrapper = shallowMount(HelloDeveloper, {
       propsData: {
+        ...DEFAULT_PROPS,
         ...props
       },
-      localVue,
       store
     })
   }
